d6a6ed9baf78c9a13163112b064906d507b03abc
Javier Domingo Cansino <javier@jinnapp.com>
2017-07-19 10:21:40 +0100

    Improved Bot to support and update all data, still need to do some accessible querying in the data

--- sflack/bot.py
+++ sflack/bot.py
@@ -16,47 +16,87 @@ def match(expression: str, message: str):
 
 
 class Event(dict):
-    def __init__(self, *args, bot, **kwargs):
-        super().__init__(*args, **kwargs)
+    def __init__(self, bot):
+        super().__init__()
         self.bot = bot
 
     def __getattr__(self, item):
-        return self[item]
+        try:
+            return self[item]
+        except:
+            raise AttributeError()
+
+    @classmethod
+    def from_dict(cls, event_dict, bot):
+        event = cls(bot=bot)
+        event.update(event_dict)
+        return event
+
+    @classmethod
+    def from_event(cls, event):
+        new_event = cls(bot=event.bot)
+        new_event.update(event)
+        return new_event
+
+
+class MessageEvent(Event):
+    async def say(self, text):
+        # Say something in the same channel as the message
+        await self.bot.slack_api.write_to(self.channel, text)
+
+    async def tell(self, text):
+        # Say something to the sender
+        await self.bot.slack_api.write_to(self.user, text)
 
 
 class Bot:
-    def __init__(self, names, **slack_api_kwargs):
+    def __init__(self, bot_name, spaced_names=True, **slack_api_kwargs):
+        if isinstance(bot_name, str):
+            bot_name = [bot_name]
         self.commands = {}
         self.slack_api = SlackAPI(**slack_api_kwargs)
-        self.names = names
+        self.bot_names = bot_name
+        self.spaced_names = spaced_names
+        self.expression_values = {'bot_name': fr'(?P<bot_name>{"|".join(bot_name)})'}
 
-    def add_bot_command(self, expr, f):
+    def add_name_lead_command(self, expr, f):
         assert callable(f)
-        self.commands[expr] = f
+        if self.spaced_names:
+            expression = '^{bot_name} ' + expr + '$'
+        else:
+            expression = '^{bot_name}' + expr + '$'
+        self.add_message_handler(expression, f)
+
+    def add_message_handler(self, rexpression, func):
+        regex_expression = re.compile(rexpression)
+        self.commands[regex_expression] = func
 
     async def _handle_message(self, event):
-        message_split = event.text.split(' ', 1)
-        if len(message_split) == 1:
-            logger.error('No matching')
-            return
-        name, text = message_split
-        if name not in self.names:
-            logger.error('No matching')
-            return
+        already_matched = False
         for expression, function in self.commands.items():
-            if match(expression=expression, message=text):
-                asyncio.ensure_future(function(event))
+            expression = expression.format(**self.expression_values)
+            if not match(expression=expression, message=event.text):
+                logger.debug(f'{event.text} didn\'t match {expression}')
+                continue
+            if already_matched:
+                logger.warning(f'Skipping already executed {event} (Would have been {function})')
+                continue
+            already_matched = True
+            logger.debug(f'Executing {event} in {function}')
+            asyncio.ensure_future(function(event))
         else:
-            logger.error('No matching')
+            logger.debug(f'No matching {event}')
 
     async def _handle_event(self, event):
         if event.type != 'message':
             return
-        await self._handle_message(event)
+        if getattr(event, 'subtype', None):
+            return
+        await self._handle_message(MessageEvent.from_event(event=event))
 
     async def run_forever(self):
         async for event in self.slack_api.rtm_api_consume():
-            ev = Event(event, bot=self)
+            ev = Event.from_dict(event, bot=self)
             await self._handle_event(event=ev)
 
     def start(self, event_loop: AbstractEventLoop = None):
