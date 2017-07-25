a83e1cc8ddbd9540b4b629a9e940ebfb9064d898
Javier Domingo Cansino <javier@jinnapp.com>
2017-07-25 12:04:16 +0100

    Making less confusing interface

--- slackery/bot.py
+++ slackery/bot.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, print_function, unicode_literals
-import argparse
 
 import asyncio
 import logging
 import re
 from asyncio.events import AbstractEventLoop
+from inspect import iscoroutinefunction
 
 from slackery.http import SlackAPI
 
@@ -13,49 +13,59 @@ logger = logging.getLogger(__name__)
 
 
 def match(expression: str, message: str):
-    return re.match(expression, message)
+    return re.fullmatch(expression, message)
 
 
-class Event(dict):
-    def __init__(self, bot):
-        super().__init__()
+class Event:
+    affinity = None
+
+    def __init__(self, content, bot):
+        self.content = content
         self.bot = bot
 
-    def __getattr__(self, item):
-        try:
-            return self[item]
-        except:
-            raise AttributeError()
-
     @classmethod
-    def from_dict(cls, event_dict, bot):
-        event = cls(bot=bot)
-        event.update(event_dict)
-        return event
-
-    @classmethod
-    def from_event(cls, event):
-        new_event = cls(bot=event.bot)
-        new_event.update(event)
-        return new_event
+    def from_obj(cls, content, bot):
+        for cl in cls.__subclasses__():
+            if cl.affinity == content['type']:
+                return cl(content, bot)
+        else:
+            return cls(content, bot)
 
 
 class MessageEvent(Event):
+    affinity = 'message'
+
     async def say(self, text: str, to: str = None):
         # Say something in the same channel as the message
         if to is None:
-            to = self.channel
+            to = self.content['channel']
         await self.bot.slack_api.write_to(to, text)
 
     async def tell(self, text: str, to: str = None):
         if to is None:
-            to = self.user
+            to = self.content['user']
         # Say something to the sender
         await self.bot.slack_api.write_to(to, text)
 
     @property
     def sender(self):
-        return self.bot.slack_api.get_user_by_id(self.user)
+        return self.bot.slack_api.get_user_by_id(self.content['user'])
+
+    @property
+    def text(self):
+        return self.content['text']
+
+    @property
+    def matches(self):
+        if not hasattr(self, '_matches'):
+            self._matches = {}
+        return self._matches
+
+    @matches.setter
+    def matches(self, matches):
+        if hasattr(self, '_matches'):
+            raise ValueError('Matches can only be set once')
+        self._matches = matches
 
 
 class Bot:
@@ -71,37 +81,40 @@ class Bot:
     def add_name_lead_command(self, expr, f):
         assert callable(f)
         if self.spaced_names:
-            expression = '^{bot_name} ' + expr + '$'
+            expression = '{bot_name} ' + expr  # To be formatted later
         else:
-            expression = '^{bot_name}' + expr + '$'
+            expression = '{bot_name}' + expr  # To be formatted later
         self.add_message_handler(expression, f)
 
     def add_message_handler(self, rexpression, func):
+        assert iscoroutinefunction(func), f'Handler for {rexpression} needs to be coroutine ({func.__name__})'
         self.commands[rexpression] = func
 
     async def _handle_message(self, event: MessageEvent):
         already_matched = False
-        for expression, function in self.commands.items():
+        for expression, func in self.commands.items():
             expression = expression.format(**self.expression_values)
-            if not match(expression=expression, message=event.text):
+            message_match = match(expression=expression, message=event.text)
+            if not message_match:
                 logger.debug(f'Text `{event.text}` didn\'t match `{expression}`')
                 continue
             if already_matched:
-                logger.warning(f'Skipping already executed {event} (Would have been {function})')
+                logger.warning(f'Skipping already executed {event} (Would have been {func})')
                 continue
             already_matched = True
-            logger.debug(f'Executing {event} in {function}')
-            self.run_event(function=function, event=event)
+            event.matches = message_match.groupdict()
+            logger.debug(f'Executing {event} in {func}')
+            self.run_event(function=func, event=event)
         else:
             logger.debug(f'No matching {event}')
 
-    async def _handle_event(self, event):
-        if event.type != 'message':
+    async def _handle_event(self, event: Event):
+        if not isinstance(event, MessageEvent):
             return
-        if getattr(event, 'subtype', None):
+        if 'subtype' in event.content:
             return
         asyncio.ensure_future(
-            self._handle_message(MessageEvent.from_event(event=event))
+            self._handle_message(event=event)
         )
 
     async def run_forever(self):
@@ -109,7 +122,7 @@ class Bot:
         while continue_running:
             try:
                 async for event in self.slack_api.rtm_api_consume():
-                    ev = Event.from_dict(event, bot=self)
+                    ev = Event.from_obj(content=event, bot=self)
                     await self._handle_event(event=ev)
             except Exception as e:
                 continue_running = await self.internal_exception_handler(e)
@@ -118,15 +131,15 @@ class Bot:
         logger.error('Internal exception handled', exc_info=exception)
         return True
 
-    async def run_event(self, function, event):
+    async def run_event(self, func, event):
         try:
-            await function(event)
+            await func(event)
         except Exception as exception:
-            await self.handle_bot_exception(function, event, exception)
+            await self.handle_bot_exception(func, event, exception)
 
-    async def handle_bot_exception(self, function, event, exception):
-        logger.exception(f'Failed running {event} in {function}')
-        await event.say(f':boom:... Houston we found a problem: `{exception}`')
+    async def handle_bot_exception(self, func, event, exception):
+        logger.exception(f'Failed running {event} in {func}')
+        await event.say(f':boom:... Houston we found a problem: ```{exception}```')
 
     def start(self, event_loop: AbstractEventLoop = None):
         return asyncio.ensure_future(self.run_forever(), loop=event_loop)
