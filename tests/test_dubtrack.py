7f832324a75637f700f66eaef9761d2acbc7174c
Javier Domingo Cansino <javierdo1@gmail.com>
2018-03-14 22:47:24 +0000

    tests: adding channel test to dubtrack

--- tests/test_dubtrack.py
+++ tests/test_dubtrack.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import, print_function, unicode_literals
 
 import unittest.mock as mock
 
+import asynctest as am
+import pytest
+
 from abot import dubtrack
 
 
@@ -14,12 +17,28 @@ def test_dubtrack_object():
     assert dub_obj._data == data
 
 
-def test_dubtrack_channel():
+@pytest.mark.asyncio
+async def test_dubtrack_channel():
     data, backend = mock.MagicMock(), mock.MagicMock()
 
     channel = dubtrack.DubtrackChannel(data, backend)
-
     backend._register_user.assert_called_once_with(data)
 
+    backend.dubtrackws.say_in_room = am.CoroutineMock()
+    await channel.say('hello')
+    backend.dubtrackws.say_in_room.assert_called_once_with('hello')
 
+    backend.dubtrack_id = None
+    with pytest.raises(ValueError):
+        await channel.say('alo')
 
+    backend.reset_mock()
+
+    user = mock.MagicMock()
+    backend.dubtrack_users = [user]
+
+    assert channel.entities == [backend._get_entity.return_value], backend.mock_calls
+
+    backend._get_entity.assert_called_once_with(user)
+
+    assert repr(channel)
